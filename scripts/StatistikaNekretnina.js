d0828e5db27ccd4b79af84daf576f875e6bbf419
Ivona <ijozic1@etf.unsa.ba>
2024-11-26 00:11:51 +0100

    Metoda init

---
+++ scripts/StatistikaNekretnina.js
@@ -0,0 +1,10 @@
+let StatistikaNekretnina = function (){
+    const listaNekretnina = nekretnine.listaNekretnina;
+    const listaKorisnika = nekretnine.listaKorisnika;
+
+    let spisakNekretnina = SpisakNekretnina();
+
+    let init = function(listaNekretnina, listaKorisnika) { 
+        spisakNekretnina.init(listaNekretnina, listaKorisnika);
+    }
+}
