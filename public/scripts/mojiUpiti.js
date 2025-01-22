6fcb95560d11651f04d141e2551830db244f45a8
Ivona <ijozic1@etf.unsa.ba>
2025-01-22 21:24:14 +0100

    Stilske izmjene za Moji upiti

--- public/scripts/mojiUpiti.js
+++ public/scripts/mojiUpiti.js
@@ -1,16 +1,23 @@
 function prikaziMojeUpite(){
     let listaUpita = PoziviAjax.getMojiUpiti((error, data) =>{
         if(error){
-            document.getElementById("upiti").innerHTML = `<p>${error}</p>`;
-            return;
+            if(error == "Not Found"){
+                //ako je prazna lista upita
+                document.getElementById("upiti").innerHTML = `<div class="greske"><p>Niste poslali nijedan upit za nekretnine.</p>`;
+                return;
+            }
+            else if(error == "Unauthorized"){
+                document.getElementById("upiti").innerHTML = `<div class="greske"><p>Morate biti prijavljeni kako biste vidjeli svoje upite.</p></div>`;
+                return;
+            }
         }
-        if(data.length == 0){
+        /*if(data.length == 0){
             document.getElementById("upiti").innerHTML = "<p>Niste poslali nijedan upit za nekretnine.</p>";
             return;
-        }
+        }*/
         let table = "<table>";
         for(let i = 0; i < data.length; i++){
-            table += `<tr><td>Nekretnina ${data[i].id_nekretnine}: ${data[i].tekst_upita}</td></tr>`;
+            table += `<tr><td><div class = "upit">Nekretnina ${data[i].id_nekretnine}: ${data[i].tekst_upita}</div></td></tr>`;
         }
         table += "</table>";
         document.getElementById("upiti").innerHTML = table;
